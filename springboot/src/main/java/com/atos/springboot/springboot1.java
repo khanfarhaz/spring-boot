7866e83e5f3cfe150b692bb44fb22f040107822e
khanfarhaz <92144622+khanfarhaz@users.noreply.github.com>
2021-12-07 18:21:49 +0530

    Add files via upload

---
+++ springboot/src/main/java/com/atos/springboot/springboot1.java
@@ -0,0 +1,13 @@
+package com.atos.springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class springboot1 
+{
+  public static void main(String[]args)
+  {
+	  SpringApplication.run(springboot1.class,args);
+  }
+}
